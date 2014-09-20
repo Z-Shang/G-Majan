f39e6aa534b1fd49ac9c5f514ed63c1fd27a4275
Z-Shang <shangzhanlin@gmail.com>
2014-09-19 23:11:55 -0500

    add main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  Entrance of GMajan
+ *
+ *        Version:  1.0
+ *        Created:  09/19/2014 10:47:12 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Z.Shang (), shangzhanlin@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <gtk/gtk.h>
+
+int main(int argc, char *argv[]){
+    gtk_init(&argc, &argv);
+
+
+    return 0;
+}
