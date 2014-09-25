d91d9ff4ed25219a6444e1fa88ad4001b99bdb2e
Z-Shang <shangzhanlin@gmail.com>
2014-09-25 02:36:51 -0500

    Update hai.rkt

--- client/src/hai.rkt
+++ client/src/hai.rkt
@@ -23,10 +23,12 @@
   (struct hai (color number aka)
           ;; Color : 1   2   3   4    5       6 
           ;; Color : Man Sou Pin Hana Suushii Sangen
+          ;; Color : 万  索  饼  花牌 四喜牌  三元牌    （花牌包括春夏秋冬和梅兰竹菊）
           ;; Number: 1~9 1~9 1~9 1~8  1~4     1~3
           ;; For Hana: 1~8 are : Chun Xia Qiu Dong Mei Lan Zhu Ju
           ;; For Suushii: 1~4 are : Ton Nan Shaa Pei
-          ;; For Sangen: 1~3 are : Haku Hatsu Chun
+          ;; For Sangen: 1~3 are : Haku Hatsu Chun      NRM （白 发 中）
+          ;; For Sangen: 1~3 are : Chun Hatsu Haku      CNSM（中 发 白）
           ;; The tiles are arranged in the order of color above
           ;; Aka is the indicator of AKA DORA only for NRM
           )
